50e2be07aed3f2e442c8ad82ec17b90aa51fe3ad
Rodrigo Morais <rcastromorais01@gmail.com>
2023-11-13 20:36:38 -0300

    removing comments

--- src/learn/ContainsDuplicate.java
+++ src/learn/ContainsDuplicate.java
@@ -4,28 +4,28 @@ public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) { // if any two elements are the same, return true
+                if (nums[i] == nums[j]) {
                     return true;
                 }
             }
         }
-        return false; // if no duplicates are found, return false
+        return false;
     }
 
     public static void main(String[] args) {
         ContainsDuplicate solution = new ContainsDuplicate();
 
         int[] nums1 = {1, 2, 3, 4};
-        System.out.println(solution.containsDuplicate(nums1)); // Expected output: false
+        System.out.println(solution.containsDuplicate(nums1));
 
         int[] nums2 = {1, 2, 3, 1};
-        System.out.println(solution.containsDuplicate(nums2)); // Expected output: true
+        System.out.println(solution.containsDuplicate(nums2));
 
         int[] nums3 = {};
-        System.out.println(solution.containsDuplicate(nums3)); // Expected output: false
+        System.out.println(solution.containsDuplicate(nums3));
 
         int[] nums4 = {1, 1, 1, 1};
-        System.out.println(solution.containsDuplicate(nums4)); // Expected output: true
+        System.out.println(solution.containsDuplicate(nums4));
     }
 }
 }
