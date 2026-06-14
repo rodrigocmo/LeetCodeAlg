a59b48042b74fbe140d57ea538c036fcae88e815
Rodrigo Morais <rcastromorais01@gmail.com>
2026-06-14 10:37:15 -0300

    two sum and palindrome otimizados

--- src/patterns/TwoPointers/TwoSum.java
+++ src/patterns/TwoPointers/TwoSum.java
@@ -1,7 +1,30 @@
 package patterns.TwoPointers;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+
+    public static void main(String[] args) {
+
+    }
+        public static int[] twoSumOtimizada(int[] numbers, int target) {
+            int left = 0;
+            int right = numbers.length - 1;
+
+            while (left < right) {
+                int sum = numbers[left] + numbers[right];
+
+                if (sum == target) {
+                    return new int[]{left + 1, right + 1}; // 1-based
+                } else if (sum < target) {
+                    left++;   // precisa de valor maior
+                } else {
+                    right--;  // precisa de valor menor
+                }
+            }
+            return new int[]{};
+        }
+
+
+    public static int[] twoSumOk(int[] nums, int target) {
         int aux = 0;
         int[] fim= new int[2];
 
@@ -16,3 +39,4 @@ public class TwoSum {
 
     }
 }
+
--- src/patterns/TwoPointers/ValidPalindrome.java
+++ src/patterns/TwoPointers/ValidPalindrome.java
@@ -1,4 +1,32 @@
 package patterns.TwoPointers;
-
 public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("rar"));
+
+    }
+
+    public static void withStringBuilder(){
+        StringBuilder sb = new StringBuilder("rar");
+        System.out.println(sb.toString().contentEquals(sb.reverse()));
+    }
+
+    public static boolean isPalindrome(String s) {
+            int left = 0, right = s.length() - 1;
+
+            while (left < right) {
+                while (left < right && !Character.isLetterOrDigit(s.charAt(left)))//pula o ponteiro em caso de nao ser letra
+                    left++;
+                while (left < right && !Character.isLetterOrDigit(s.charAt(right)))// mesma coisa
+                    right--;
+
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))// faz a comparacao verdadeira
+                    return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
 }
